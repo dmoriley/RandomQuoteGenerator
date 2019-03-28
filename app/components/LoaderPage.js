77366b37a0b5996c33db5fb46b31728a132d9828
David O'Riley <DavidO@Flextrack.ca>
2019-03-28 09:49:11 -0400

    Changed file stucture so build script will work

--- app/components/LoaderPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div className="loader">
-    <img className="loader__img" src="https://cdn.pixabay.com/photo/2015/02/22/17/56/loading-645268_960_720.jpg"/>
-  </div>
-);
